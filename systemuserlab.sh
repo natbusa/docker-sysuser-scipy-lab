7088a278f1aba741449d993a93dc5e758030a6ca
natalinobusa <natalino.busa@gmail.com>
2017-03-24 13:19:27 +0100

    removed notebook_dir settings

--- systemuserlab.sh
+++ systemuserlab.sh
@@ -2,6 +2,5 @@
 
 sh /srv/singleuser/systemuser.sh \
   --SingleUserNotebookApp.default_url=/lab \
-  --SingleUserNotebookApp.notebook_dir=/home/{username} \
   --SingleUserNotebookApp.disable_user_config=True \
   $@
